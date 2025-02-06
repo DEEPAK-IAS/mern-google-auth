d6295ea455f79eb9ba43abba789ef117d9be1c88
DEEPAK-IAS <deepaksurya2002dgl@gmail.com>
2025-02-06 11:42:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,11 @@ const path = require("path");
 const authRoute = require("./routes/auth.route");
 const userRoute = require("./routes/user.route");
 const adminRoute = require("./routes/admin.route");
+
 mongodb.connect();
 app.use(express.json())
 app.use(cookieParser());
+
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/admin", adminRoute);
